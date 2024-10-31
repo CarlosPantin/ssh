052983feb2c570cb6b270ebeb7199286b68dd809
carlospantin <carlospantin@live.com.mx>
2024-10-31 11:09:50 +0200

    US1-Test2

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,12 +1,22 @@
-
 class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
+        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
 
-    def evaluate(self, cell: str) -> int:
-        return int(self._cells[cell])
+    def get(self, cell: str) -> str:
+        return self._cells.get(cell, '')
 
+    def evaluate(self, cell: str) -> int | str:
+        value = self.get(cell)
+        if value.isdigit():
+            return int(value)
+        else:
+            try:
+                float(value)  # Check if it can be a valid float
+                return "#Error"
+            except ValueError:
+                return value
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -6,5 +6,10 @@ class TestSpreadSheet(TestCase):
 
     def test_evaluate_valid_integer(self):
         spreadsheet = SpreadSheet()
-        spreadsheet.set("A1" , "1")
-        self.assertEqual(1, spreadsheet.evaluate("A1"))
+        spreadsheet.set("A1","1")
+        self.assertEqual(1,spreadsheet.evaluate("A1"))
+
+    def test_evaluate_non_valid_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1","1.5")
+        self.assertEqual("#Error",spreadsheet.evaluate("A1"))
